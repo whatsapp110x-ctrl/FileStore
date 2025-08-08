29d0fd4ac4739ffe81242594df2b46add31757ae
whatsapp110x-ctrl <whatsapp110x@gmail.com>
2025-08-08 23:32:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bot import Bot
 import pyrogram.utils
+from keep_alive import keep_alive  # Added
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 if __name__ == "__main__":
+    keep_alive()  # Start the web server
     Bot().run()
