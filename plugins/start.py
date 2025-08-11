ebbaa5127f8dc2cd3b7f19b8e2c0584061c65866
whatsapp110x-ctrl <whatsapp110x@gmail.com>
2025-08-11 11:41:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -97,10 +97,8 @@ async def start_command(client: Client, message: Message):
  
         codeflix_msgs = []
         for msg in messages:
-            caption = (CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, 
-                                             filename=msg.document.file_name) if bool(CUSTOM_CAPTION) and bool(msg.document)
-                       else ("" if not msg.caption else msg.caption.html))
-            reply_markup = msg.reply_markup if DISABLE_CHANNEL_BUTTON else None
+            caption = ""  # Always blank to remove any ad text from forwarded messages
+            reply_markup = None  # No buttons forwarded
             try:
                 copied_msg = await msg.copy(
                     chat_id=message.from_user.id,
